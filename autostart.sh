5248781a5cdd2bb207afc2a6dc62e35faefa6e53
nat35lint <nahtenolird@proton.me>
2022-06-27 18:53:02 +0800

    autostart

--- autostart.sh
+++ autostart.sh
@@ -3,3 +3,7 @@ nitrogen --restore &
 picom --config ~/.config/dwm/picom.conf --experimental-backends&
 sxhkd -c ~/.config/dwm/sxhkdrc &
 sh ~/.config/dwm/sbar/sbar &
+lxsession $
+# MPD daemon start (if no other user instance exists)
+[ ! -s ~/.config/mpd/pid ] && mpd $
+

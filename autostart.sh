f3179fa03198a034b0091f2972b80b4ce4220fbf
nat35lint <nahtenolird@proton.me>
2022-07-10 19:16:01 +0800

    cleaning

--- autostart.sh
+++ autostart.sh
@@ -2,7 +2,7 @@
 nitrogen --restore &
 picom --config ~/.config/dwm/picom.conf --experimental-backends &
 sxhkd -c ~/.config/dwm/sxhkdrc &
-sh ~/.config/dwm/sbar/sbar &
+sh ~/.config/dwm/sbar &
 #sh ~/.config/dwm/keyswap &
 lxsession &
 mpDris2 &
